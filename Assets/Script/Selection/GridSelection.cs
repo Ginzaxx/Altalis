45692a8d7e27c64bc476fc5f24b47d4a2864168f
Ginzaxx <First.ginzax@gmail.com>
2025-08-25 14:36:36 +0700

    Selection system with click and drag

--- Assets/Script/Selection/GridSelection.cs
+++ Assets/Script/Selection/GridSelection.cs
@@ -1,43 +1,93 @@
 using UnityEngine;
+using System.Collections.Generic;
 
-public class GridSelection : MonoBehaviour
+public class DragSelection : MonoBehaviour
 {
-    [SerializeField] private GridCursor cursor;
     [SerializeField] private Camera cam;
+    [SerializeField] private RectTransform selectionBoxUI; // UI overlay box
 
-    private GameObject selectedObject;
+    private Vector2 startPos;
+    private Vector2 endPos;
+
+    private List<GameObject> selectedObjects = new List<GameObject>();
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            SelectAtCursor();
+            startPos = Input.mousePosition;
+            selectionBoxUI.gameObject.SetActive(true);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            endPos = Input.mousePosition;
+            DrawSelectionBox();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            SelectObjects();
+            selectionBoxUI.gameObject.SetActive(false);
         }
     }
 
-    void SelectAtCursor()
+    void DrawSelectionBox()
     {
-        // Reset highlight kalau ada object lama
-        if (selectedObject != null)
+        Canvas parentCanvas = selectionBoxUI.GetComponentInParent<Canvas>();
+        RectTransform canvasRect = parentCanvas.transform as RectTransform;
+
+        Vector2 localStartPos;
+        Vector2 localEndPos;
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRect, startPos, parentCanvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : cam, out localStartPos);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRect, endPos, parentCanvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : cam, out localEndPos);
+
+        Vector2 boxCenter = (localStartPos + localEndPos) / 2f;
+        Vector2 boxSize = new Vector2(Mathf.Abs(localEndPos.x - localStartPos.x), Mathf.Abs(localEndPos.y - localStartPos.y));
+
+        selectionBoxUI.anchoredPosition = boxCenter;
+        selectionBoxUI.sizeDelta = boxSize;
+    }
+
+
+
+    void SelectObjects()
+    {
+        // Reset warna object lama
+        foreach (var obj in selectedObjects)
         {
-            SpriteRenderer oldSr = selectedObject.GetComponent<SpriteRenderer>();
-            if (oldSr != null) oldSr.color = Color.white;
+            if (obj != null)
+            {
+                SpriteRenderer sr = obj.GetComponent<SpriteRenderer>();
+                if (sr != null) sr.color = Color.white;
+            }
+        }
+        selectedObjects.Clear();
+
+        // Buat rect world dari mouse drag
+        Vector3 p1 = cam.ScreenToWorldPoint(startPos);
+        Vector3 p2 = cam.ScreenToWorldPoint(endPos);
+
+        float minX = Mathf.Min(p1.x, p2.x);
+        float maxX = Mathf.Max(p1.x, p2.x);
+        float minY = Mathf.Min(p1.y, p2.y);
+        float maxY = Mathf.Max(p1.y, p2.y);
+
+        Collider2D[] hits = Physics2D.OverlapAreaAll(new Vector2(minX, minY), new Vector2(maxX, maxY));
+
+        foreach (Collider2D hit in hits)
+        {
+            if (hit.CompareTag("Selectable"))
+            {
+                selectedObjects.Add(hit.gameObject);
+
+                // Highlight kuning
+                SpriteRenderer sr = hit.GetComponent<SpriteRenderer>();
+                if (sr != null) sr.color = Color.yellow;
+            }
         }
 
-        Vector3 worldPos = cursor.CurrentCellCenter;
-        RaycastHit2D hit = Physics2D.Raycast(worldPos, Vector2.zero);
-
-        if (hit.collider != null && hit.collider.CompareTag("Selectable"))
-        {
-            selectedObject = hit.collider.gameObject;
-            Debug.Log("Selected: " + selectedObject.name);
-
-            SpriteRenderer sr = selectedObject.GetComponent<SpriteRenderer>();
-            if (sr != null) sr.color = Color.yellow; // highlight
-        }
-        else
-        {
-            selectedObject = null;
-        }
+        Debug.Log("Selected " + selectedObjects.Count + " objects");
     }
 }
