466c86c725115e6cec97e603a99774127519c1e5
LogicLover50 <hpaentser.50@gmail.com>
2025-10-21 05:09:06 +0700

    Camera Fix

--- Assets/Script/Camera/CameraFollow2D.cs
+++ Assets/Script/Camera/CameraFollow2D.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraFollow2D : MonoBehaviour
 {
@@ -16,7 +17,9 @@ public class CameraFollow2D : MonoBehaviour
     private float manualYOffset = 0f;
     public float maxUpOffset = 3f;
     public float maxDownOffset = -2f;
-    public float returnSpeed = 3f; // kecepatan kamera kembali ke posisi semula
+    public float returnSpeed = 3f;
+    bool cameraMoving = false;
+
 
     [Header("World Borders (Manual)")]
     public bool useManualBorders = true;
@@ -25,7 +28,6 @@ public class CameraFollow2D : MonoBehaviour
 
     [Header("World Borders (Collider)")]
     public BoxCollider2D bounds;
-
     private Vector3 minBounds;
     private Vector3 maxBounds;
     private float camHalfHeight;
@@ -48,19 +50,8 @@ public class CameraFollow2D : MonoBehaviour
     {
         if (target == null) return;
 
-        bool holdingUp = Input.GetKey(KeyBindings.CameraUpKey);
-        bool holdingDown = Input.GetKey(KeyBindings.CameraDownKey);
-
-        // Jika sedang menahan tombol W/S
-        if (holdingUp)
-        {
-            manualYOffset += manualMoveSpeed * Time.deltaTime;
-        }
-        else if (holdingDown)
-        {
-            manualYOffset -= manualMoveSpeed * Time.deltaTime;
-        }
-        else
+        // Jika tidak sedang menahan tombol W/S
+        if (!cameraMoving)
         {
             // Jika dilepas, smooth kembali ke posisi 0
             manualYOffset = Mathf.Lerp(manualYOffset, 0f, Time.deltaTime * returnSpeed);
@@ -92,5 +83,11 @@ public class CameraFollow2D : MonoBehaviour
 
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, clampedPosition, smoothSpeed);
         transform.position = smoothedPosition;
+    }
+
+    public void MoveCamera(InputAction.CallbackContext context)
+    {
+        cameraMoving = true;
+        manualYOffset = context.ReadValue<Vector2>().y * manualMoveSpeed * Time.deltaTime;
     }
 }
