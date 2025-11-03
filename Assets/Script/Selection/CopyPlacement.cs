69aa36a8f54a62e4fb57162a08cf4bc2a4536b83
ArdhenisAflah <aflahdenis9@gmail.com>
2025-11-03 21:12:37 +0700

    Revert "Merge branch 'Hans' of https://github.com/Ginzaxx/Altalis into Hans"
    This reverts commit 8b0cdc1a9256e3b15b2415d92cdd1e73ef9893eb, reversing
    changes made to b28fa928083b05c32465e49fda5941e0f6a4511e.

--- Assets/Script/Selection/CopyPlacement.cs
+++ Assets/Script/Selection/CopyPlacement.cs
@@ -10,6 +10,7 @@ public class CopyPlacement : MonoBehaviour
     [SerializeField] private GridSelection gridSelection;
     [SerializeField] private GridCursor gridCursor;
     [SerializeField] private Tilemap targetTilemap;
+    [SerializeField] private GameModeManager gameModeManager;
 
     [Header("VFX")]
     [SerializeField] private GameObject placeVfxPrefab;
@@ -128,8 +129,11 @@ public class CopyPlacement : MonoBehaviour
     {
         if (CancelKey.performed)
         {
-            // Debug.Log("Pressing Cancel");
-            CancelPlacement();
+            Debug.Log("Pressing Cancel");
+            if (isPlacing)
+                CancelPlacement();
+            else
+                Debug.Log("Not Placing");
         }
     }
 
